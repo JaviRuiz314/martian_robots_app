016d8153efa791959f3a1aeb4c98ac3a190d74b4
Javier Ruiz de Valdivia <javiruiz314@gmail.com>
2021-02-14 11:36:29 +0100

    create robot on database

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ require('dotenv').config();
 
 app.listen(process.env.PORT, () => {
     console.log(`App running on port ${process.env.PORT}...`);
-})
+});
+
+
--- src/service/robotService.js
+++ src/service/robotService.js
@@ -1,14 +1,32 @@
 'use strict'
 
+const models = require('../models');
 class RobotService {
     constructor(name, inPosition) {
         console.log(name);
         console.log(inPosition);
+        return await this.createRobot(name, inPosition);
     }
 
-    getRobotInfo(){}
-    createRobot(){}
-    updateStatus(){}
+    getRobotInfo() { }
+    async createRobot(name, inPosition) {
+        const newRobot = await models.Robot.create({
+            Name: name,
+            Status: 'ACTIVE'
+        });
+        newRobot.inPosition = inPosition;
+        return newRobot;
+    }
+    updateStatus(id, status) {
+        await models.Robot.update(
+          { Status: status},
+          {
+              where: {
+                  Id: id
+              }
+          }  
+        );
+    }
 }
 
 module.exports = {
