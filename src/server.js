d09a703d78d31366185f9b9867a186fbd91586c7
Javier Ruiz de Valdivia <javiruiz314@gmail.com>
2021-02-14 17:38:45 +0100

    modify server.js

--- src/server.js
+++ src/server.js
@@ -9,10 +9,10 @@ app.use(bodyParser.json({ limit: '2mb' }));
 app.use(bodyParser.urlencoded({ limit: '2mb', extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static('./index.html'));
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve('./', "index.html"), err => console.log(err));
-});
+//app.use(express.static('./index.html'));
+//app.get("/", (req, res) => {
+//    res.sendFile(path.resolve('./', "index.html"), err => console.log(err));
+//});
 
 app.use(routes.Robot);
 
