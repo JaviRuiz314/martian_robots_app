e8bd94fdf2125d5b251a42f6e18c0775f80a6db9
Javier Ruiz de Valdivia <javiruiz314@gmail.com>
2021-02-16 16:47:21 +0100

    refactor robot class

--- src/controller/robotController.js
+++ src/controller/robotController.js
@@ -3,22 +3,10 @@
 const { RobotService } = require('../service/robotService');
 
 
-function _parseStingIntoIntArray(string) {
-	const array = string.split(',');
-
-	for (let i of array) {
-		i = parseInt(i);
-	}
-	return array;
-}
 async function createNewRobot(req, res) {
 	try {
-		if (typeof (req.body.inPosition) !== "array") {
-			req.body.inPosition = _parseStingIntoIntArray(req.body.inPosition);
-		}
-		const robot = new RobotService(req.body.name, req.body.inPosition);
-		await robot.constructor.createRobot();
-		res.status(200);
+		const robot = await createRobot(req.body.name);
+		res.status(200).send('New robot created succesfully with name: ' + robot.dataValues.Name);
 	} catch (err) {
 		console.log('createNewRobot unexpected error: ' + err.toString());
 		res.send(500).send('Server error');
--- src/service/robotService.js
+++ src/service/robotService.js
@@ -4,39 +4,33 @@ const
 	models = require('../models'),
 	util = require('../shared/util');
 
-class RobotService {
 
-	constructor(name, inPosition) {
-		this.name = name,
-			this.inPosition = inPosition
-	}
-
-	static async createRobot() {
-		const latestMarsTerrain = await models.MarsTerrain.findOne({
-			order: ['Id', 'DESC'],
-		});
-		const newRobot = await models.Robot.create({
-			Name: this.name,
-			Status: util.CREATED_ROBOT_STATUS
-		});
-		await models.MarsTerrainToRobot.create({
-			MarsTerrainId: latestMarsTerrain.dataValues.Id,
-			RobotId: newRobot.dataValues.Id
-		});
-		return newRobot;
-	}
-	async updateStatus(id, status) {
-		await models.Robot.update(
-			{ Status: status },
-			{
-				where: {
-					Id: id
-				}
+async function createRobot(name) {
+	const latestMarsTerrain = await models.MarsTerrain.findOne({
+		order: ['Id', 'DESC'],
+	});
+	const newRobot = await models.Robot.create({
+		Name: name,
+		Status: util.CREATED_ROBOT_STATUS
+	});
+	await models.MarsTerrainToRobot.create({
+		MarsTerrainId: latestMarsTerrain.dataValues.Id,
+		RobotId: newRobot.dataValues.Id
+	});
+	return newRobot;
+}
+async function updateStatus(id, status) {
+	await models.Robot.update(
+		{ Status: status },
+		{
+			where: {
+				Id: id
 			}
-		);
-	}
+		}
+	);
 }
 
 module.exports = {
-	RobotService
+	createRobot,
+	updateStatus
 }
