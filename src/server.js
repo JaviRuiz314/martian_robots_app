2d76f2556fa6ad0fc2ff57f81f1a54588bb40ed5
Javier Ruiz de Valdivia <javiruiz314@gmail.com>
2021-02-19 14:00:01 +0100

    serve help on root url

--- src/server.js
+++ src/server.js
@@ -1,18 +1,21 @@
 const
 	express = require('express'),
 	app = express(),
-	path = require('path'),
+	marked = require('marked'),
 	routes = require('./routes'),
+	fs = require('fs'),
 	bodyParser = require('body-parser');
 
 app.use(bodyParser.json({ limit: '2mb' }));
 app.use(bodyParser.urlencoded({ limit: '2mb', extended: false }));
 app.use(bodyParser.json());
 
-//app.use(express.static('./index.html'));
-//app.get("/", (req, res) => {
-//    res.sendFile(path.resolve('./', "index.html"), err => console.log(err));
-//});
+app.get('/', function (req, res) {
+	let
+		path = './README.md',
+		file = fs.readFileSync(path, 'utf8');
+	res.send(marked(file.toString()));
+});
 
 app.use(routes.Robot);
 app.use(routes.ObjectTracker);
